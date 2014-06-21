27e91aa1bc2ea187dca0418a3dc2e6d3df5daa9a
ginpei <ginpei@gmail.com>
2014-06-21 14:14:47 +0900

    typo

--- src/tab.js
+++ src/tab.js
@@ -1,11 +1,8 @@
-// Profileタブをクリック
-$('#profile-link').on('click', function(event) {
-	event.preventDefault();
-
+function switchTab() {
 	// 他のボタンを不活性化
 	$('#notification-link, #design-link').removeClass('active');
 
-	// このボタンを不活性化
+	// このボタンを活性化
 	$('#profile-link').addClass('active');
 
 	// 他のページを不可視化
@@ -13,26 +10,25 @@ $('#profile-link').on('click', function(event) {
 
 	// このページを可視化
 	$('#profile').addClass('active');
+}
+
+// Profileタブをクリック
+$('#profile-link').on('click', function(event) {
+	event.preventDefault();
+
+	switchTab();
 });
 
 // Notificationをクリック
 $('#notification-link').on('click', function(event) {
 	event.preventDefault();
 
-	$('#profile-link, #design-link').removeClass('active');;
-	$('#notification-link').addClass('active');
-
-	$('#profile, #design').removeClass('active');
-	$('#notification').addClass('active');
+	switchTab();
 });
 
 // Designタブをクリック
 $('#design-link').on('click', function(event) {
 	event.preventDefault();
 
-	$('#profile-link, #notification-link').removeClass('active');
-	$('#design-link').addClass('active');
-
-	$('#profile, #notification').removeClass('active');
-	$('#design').addClass('active');
+	switchTab();
 });
